ba3108d4a5f5e7180e765e5e7c7dbe3f184b6a6c
Richard Hallett <richard@richardhallett.com>
2018-02-09 15:00:07 +0000

    A better titelize function that covers more edge cases. This is directly taken from rails

--- lib/bolognese/string.rb
+++ lib/bolognese/string.rb
@@ -1,5 +1,6 @@
 class String
   def my_titleize
-    self.gsub(/(\b[\w']+\b|_)(.)/) { "#{$1}#{$2.upcase}" }
+    #self.gsub(/(\b[\w']+\b|_)(.)/) { "#{$1}#{$2.upcase}" }
+    self.gsub(/\b(['’]?[a-z])/) { "#{$1.capitalize}" }
   end
 end
