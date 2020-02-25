de4a2d081d6ee42dfd8c206a2c87d8052a5c4d3a
Martin Fenner <martin.fenner@datacite.org>
2020-02-25 17:08:24 +0100

    require json/ld

--- lib/bolognese/version.rb
+++ lib/bolognese/version.rb
@@ -1,3 +1,3 @@
 module Bolognese
-  VERSION = "1.5.9"
+  VERSION = "1.5.10"
 end
--- lib/bolognese/writers/rdf_xml_writer.rb
+++ lib/bolognese/writers/rdf_xml_writer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "json/ld"
+
 module Bolognese
   module Writers
     module RdfXmlWriter
