3103b1230001da77af8d4b7b8448b8a9fd188cbb
kudakwashe SIziva <kuda.siziva@datacite.org>
2026-04-24 09:22:22 +0200

    update the patch

--- lib/bolognese/citeproc_extensions.rb
+++ lib/bolognese/citeproc_extensions.rb
@@ -3,21 +3,51 @@
 # Ruby 4.0 compatibility patch for citeproc-ruby
 module CiteProc
   class Variable
+    # Add to_a method to convert Variable to array when needed
+    def to_a
+      val = @value
+      case val
+      when Array
+        val
+      when nil
+        []
+      else
+        [val]
+      end
+    end
+    
     # Delegate Enumerable methods that may not be properly forwarded in Ruby 4
     def take(n)
       to_a.take(n)
     end
     
-    def method_missing(method, *args, &block)
-      if to_a.respond_to?(method)
-        to_a.send(method, *args, &block)
-      else
-        super
-      end
+    def first
+      to_a.first
     end
     
-    def respond_to_missing?(method, include_private = false)
-      to_a.respond_to?(method, include_private) || super
+    def last
+      to_a.last
+    end
+    
+    def empty?
+      to_a.empty?
+    end
+    
+    def size
+      to_a.size
+    end
+    alias length size
+    
+    def each(&block)
+      to_a.each(&block)
+    end
+    
+    def map(&block)
+      to_a.map(&block)
+    end
+    
+    def select(&block)
+      to_a.select(&block)
     end
   end
 end
